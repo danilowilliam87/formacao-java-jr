685c3304011e01e9d1b2b6f998253ff553ca0465
Danilo William dos Reis Socorro <danilowilliam87@gmail.com>
2022-09-12 20:48:14 -0300

    feat:relacionamento user(client) e order

--- curso-spring/src/main/java/io/wdsolutions/cursospring/entities/User.java
+++ curso-spring/src/main/java/io/wdsolutions/cursospring/entities/User.java
@@ -1,12 +1,15 @@
 package io.wdsolutions.cursospring.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -21,6 +24,9 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 		super();
