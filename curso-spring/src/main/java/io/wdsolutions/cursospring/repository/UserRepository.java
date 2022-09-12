7b9142c6192410be25567ae26af34870c338cb03
Danilo William dos Reis Socorro <danilowilliam87@gmail.com>
2022-09-12 18:41:32 -0300

    feat:criação da classe de configuração de testes no ambiente Dev

--- curso-spring/src/main/java/io/wdsolutions/cursospring/repository/UserRepository.java
+++ curso-spring/src/main/java/io/wdsolutions/cursospring/repository/UserRepository.java
@@ -1,5 +1,8 @@
 package io.wdsolutions.cursospring.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +10,9 @@ import io.wdsolutions.cursospring.entities.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
-
+	
+	Optional<User> findByName(String name);
+	Optional<User> findByEmail(String email);
+	List<User> findByNameLike(String name);
+	
 }
