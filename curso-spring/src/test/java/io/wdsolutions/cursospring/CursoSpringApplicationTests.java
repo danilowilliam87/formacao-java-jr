bfe952b8c5312d996380d523d1f94048b238812a
Danilo William dos Reis Socorro <danilowilliam87@gmail.com>
2022-09-12 16:55:05 -0300

    feat:commit inicial

---
+++ curso-spring/src/test/java/io/wdsolutions/cursospring/CursoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.wdsolutions.cursospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
